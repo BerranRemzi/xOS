dc9a68b6b81988fb84303965e8d0e567924bb902
Berran <berranremzi@gmail.com>
2019-11-08 11:52:43 +0200

    Auto code format

--- src/xOS.c
+++ src/xOS.c
@@ -1,63 +1,63 @@
 #include "xOS.h"
 
 void xLoop(void) {
-  currentTime = xMillis();
+	currentTime = xMillis();
 
-  for (uint8_t i = 0; i < tasksNum; ++i) {
-    if (currentTime >= p_Task[i].previousTime  + p_Task[i].period && !isRunning) {
-      //ChangeDebugPinState(HIGH);
+	for (uint8_t i = 0; i < tasksNum; ++i) {
+		if (currentTime >= p_Task[i].previousTime + p_Task[i].period && !isRunning) {
+			//ChangeDebugPinState(HIGH);
 
-      p_Task[i].previousTime = currentTime;
-      p_Task[i].elapsedTime = p_Task[i].previousTime + p_Task[i].runtime;
-      p_Task[i].isRunning = true;
-      isRunning = true;
-      p_Task[i].TaskFunction();
-    }
-    if (currentTime >= p_Task[i].elapsedTime && p_Task[i].isRunning) {
-      //ChangeDebugPinState(LOW);
-      
-      p_Task[i].isRunning = false;
-      isRunning = false;
-    }
-  }
+			p_Task[i].previousTime = currentTime;
+			p_Task[i].elapsedTime = p_Task[i].previousTime + p_Task[i].runtime;
+			p_Task[i].isRunning = true;
+			isRunning = true;
+			p_Task[i].TaskFunction();
+		}
+		if (currentTime >= p_Task[i].elapsedTime && p_Task[i].isRunning) {
+			//ChangeDebugPinState(LOW);
+
+			p_Task[i].isRunning = false;
+			isRunning = false;
+		}
+	}
 }
 static inline void ChangeDebugPinState(bool _input) {
-  digitalWrite(cpuLoadPin, _input);
+	digitalWrite(cpuLoadPin, _input);
 }
 
-void xTaskCreate(void (*_p_Input)(void), uint32_t _period, uint32_t _runtime, uint8_t _priority) {
-  p_Task[_priority].period = _period;
-  p_Task[_priority].runtime = _runtime;
-  p_Task[_priority].priority = _priority;
-  p_Task[_priority].TaskFunction = _p_Input;
-  tasksNum++;
+void xTaskCreate(void(*_p_Input)(void), uint32_t _period, uint32_t _runtime, uint8_t _priority) {
+	p_Task[_priority].period = _period;
+	p_Task[_priority].runtime = _runtime;
+	p_Task[_priority].priority = _priority;
+	p_Task[_priority].TaskFunction = _p_Input;
+	tasksNum++;
 }
 
 void xInit(Task_t *_input) {
-  p_Task = _input;
+	p_Task = _input;
 }
 
 void xDebugPin(uint8_t _pin) {
-  cpuLoadPin = _pin;
+	cpuLoadPin = _pin;
 }
 
-void xPowerDown(uint8_t _period){
-	wdtTime+=wdtDelay[_period];
+void xPowerDown(uint8_t _period) {
+	wdtTime += wdtDelay[_period];
 }
 
-inline uint32_t xMillis(void){
+inline uint32_t xMillis(void) {
 	return wdtTime + millis();
 }
 
-bool IsRunning(void){
-	for (uint8_t i = 0; i < tasksNum; ++i){
-		if(p_Task[i].isRunning==true){
+bool IsRunning(void) {
+	for (uint8_t i = 0; i < tasksNum; ++i) {
+		if (p_Task[i].isRunning == true) {
 			return true;
 		}
 	}
 	return false;
 }
-uint32_t xWDT(){
-	
+uint32_t xWDT() {
+
 	return wdtTime;
 }
